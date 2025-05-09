538be41ec95aca4ec1e26188af9e571d94a01442
Bngs18 <angelbanegas014@gmail.com>
2025-05-09 14:52:49 -0600

    added evente onClick optional to ButtonLogin.tsx

--- src/components/atoms/ButtonLogin.tsx
+++ src/components/atoms/ButtonLogin.tsx
@@ -3,12 +3,14 @@ import '../styles/Button.css';
 type ButtonProps = {
     className?: string;
     children: React.ReactNode;
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export const ButtonLogin = ({className, children}: ButtonProps) => {
+export const ButtonLogin = ({className, children, onClick}: ButtonProps) => {
     return(
         <>
-            <button className={`p-5 border-1 rounded-2xl bg-blue-600 h-5 w-max flex items-center hover:bg-blue-500 hover:border-black ${className}`}>
+            <button className={`p-5 border-1 rounded-2xl bg-blue-600 h-5 w-max flex items-center hover:bg-blue-500 hover:border-black ${className}`}
+                    onClick={onClick}>
                 {children}
             </button>
         </>
--- src/components/organisms/LoginFom.tsx
+++ src/components/organisms/LoginFom.tsx
@@ -5,16 +5,16 @@ import { ButtonLogin } from '../atoms/ButtonLogin';
 const LoginForm = () => {
 
     return (
-        <form className='bg-blue-950 opacity-45 rounded-xl h-[400px] w-[350px] shadow-fuchsia-950 border-black border-1 flex flex-col items-center'>
+        <form className='bg-blue-950 opacity-45 rounded-xl h-[400px] w-[350px] shadow-fuchsia-950 border-black border-1 flex flex-col items-center p-10'>
             <div className=' flex justify-center p-5 my-[12px]'>
                 <label className='text-white text-[22px] font-serif'>Login</label>
             </div>
-            <div className='w-[80%]'>
+            <div className=''>
                 <InputLogin typeInput='text' preview='Username' className='my-[10px]'/>
                 <InputLogin typeInput='password' preview='Password' />
             </div>
-            <div className='w-[80%] flex items-start'><label className='text-blue-400 text-[15px] italic underline font-sans'>Forgot your password?</label></div>
-            <div className='flex gap-2 my-3 items-start w-[80%]'>
+            <div className='flex items-start w-[100%]'><label className='text-blue-400 text-[15px] italic underline font-sans'>Forgot your password?</label></div>
+            <div className='flex gap-2 my-3 items-start w-[100%]'>
                 <ButtonLogin>Login</ButtonLogin>
                 <ButtonLogin>Sign up</ButtonLogin>
             </div>
