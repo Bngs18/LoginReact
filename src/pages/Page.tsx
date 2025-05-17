a0287be253a74cfa4e35ce9a5546b166723a5e33
Bngs18 <angelbanegas014@gmail.com>
2025-05-17 12:16:44 -0600

    new version

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -3,7 +3,7 @@ import '../styles/Login.css';
 export const Page = () => {
     return (
         <>
-            <div className='bg-amber-950 w-screen h0s'>Hello World</div>
+            <div className='bg-amber-950 w-screen h-screen'>Hello World</div>
         </>
     )
 }
